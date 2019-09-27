1f38064eb27b8b5c8d870a753691a81598133dd7
Catlabuga Octavian | medneo <octavian.catlabuga@medneo.com>
2019-09-27 13:58:35 +0200

    add put methods (test)

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const morgan = require('morgan')
 const debug = require('debug')('dss')
 
 const router = Router()
+// object to store the requests
 router.__dataStore = {}
+
+console.log('data before: ' + router.__dataStore[0])
 
 const morganDebugStream = new stream.Writable({
   write: function (chunk, encoding, done) {
@@ -34,25 +37,30 @@ router.use(bodyParser.raw({ limit: '10mb', type: () => true }))
 
 const bodyDebug = debug.extend('body')
 
+//----------- post method-----------
 router.post('/data/:id', (req, res) => {
+  // id of the who ever sends the request, comes after /data/<deviceId>
   const deviceId = req.params.id
 
-  if (!router.__dataStore[deviceId]) {
+  if (!router.__dataStore[deviceId])
+  {
+    // make an empty array
     router.__dataStore[deviceId] = []
   }
-
   // log the body, using the debug body instance
   bodyDebug(req.body.toString())
 
   router.__dataStore[deviceId].push(req.body)
+  console.log('data before: ' + router.__dataStore[deviceId].length)
 
   res.statusCode = 200
   res.end()
 })
 
+//------------ get method -------------
 router.get('/data/:id', (req, res) => {
   const deviceId = req.params.id
-
+  console.log(deviceId);
   if (!router.__dataStore[deviceId] || router.__dataStore[deviceId].length === 0) {
     res.statusCode = 404
     res.end()
@@ -63,5 +71,40 @@ router.get('/data/:id', (req, res) => {
     res.end(data)
   }
 })
+
+router.put('/data/:id', (req, res) => {
+    const deviceId = req.params.id
+    if (!router.__dataStore[deviceId])
+    {
+      // make an empty array
+      router.__dataStore[deviceId] = []
+      // add a empty palceholder to message array
+      router.__dataStore[deviceId][0] = undefined;
+    }
+    // log the body, using the debug body instance
+    bodyDebug(req.body.toString())
+    router.__dataStore[deviceId][0] = req.body;
+
+    res.statusCode = 200
+    res.end()
+})
+
+router.put('/data/:id', (req, res) => {
+    const deviceId = req.params.id
+    if (!router.__dataStore[deviceId])
+    {
+      // make an empty array
+      router.__dataStore[deviceId] = []
+      // add a empty palceholder to message array
+      router.__dataStore[deviceId][0] = undefined;
+    }
+    // log the body, using the debug body instance
+    bodyDebug(req.body.toString())
+    router.__dataStore[deviceId][0] = req.body;
+
+    res.statusCode = 200
+    res.end()
+})
+
 
 module.exports = router
